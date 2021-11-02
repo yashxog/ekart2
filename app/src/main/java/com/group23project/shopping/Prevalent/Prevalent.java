1923c0ad1340b08b3126dfacebee59ed4eaa6844
yashxog <79133901+yashxog@users.noreply.github.com>
2021-11-02 12:57:33 +0530

    first commit

---
+++ app/src/main/java/com/group23project/shopping/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.group23project.shopping.Prevalent;
+
+import com.group23project.shopping.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
